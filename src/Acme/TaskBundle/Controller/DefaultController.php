df58b2f60f042dd779f0451c1bdd2f0e1c1b37f3
Aleksandar B <aleksandar89@outlook.com>
2013-07-22 15:07:14 +0200

    TaskType ubacen auto build forme

--- src/Acme/TaskBundle/Controller/DefaultController.php
+++ src/Acme/TaskBundle/Controller/DefaultController.php
@@ -4,10 +4,25 @@ namespace Acme\TaskBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Acme\TaskBundle\Entity\Task;
+use Acme\TaskBundle\Form\Type\TaskType;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+    
+    public function buildAction()
+    {
+        $task = new Task();
+        $task->setTask('Write a blog post');
+        $task->setDueDate(new \DateTime('tomorrow'));
+        
+        $form = $this->createForm(new TaskType(), $task);
+    
+        return $this->render('AcmeTaskBundle:Default:new.html.twig', array(
+            'form' => $form->createView(),
+            ));
+        
+    }
     public function newAction(Request $request)
     {
         // just setup a fresh $task object (remove the dummy data)
@@ -15,13 +30,14 @@ class DefaultController extends Controller
         $task->setTask('Write a blog post');
         $task->setDueDate(new \DateTime('tomorrow'));
         
+      
         $form = $this->createFormBuilder($task)
             ->add('task', 'text')
-            ->add('dueDate', 'date')
+            ->add('dueDate', 'date', array('widget' => 'single_text'))
             ->add('save', 'submit')
             ->add('saveAndAdd', 'submit')
             ->getForm();
-
+        
         $form->handleRequest($request);
 
         if ($form->isValid()) {
